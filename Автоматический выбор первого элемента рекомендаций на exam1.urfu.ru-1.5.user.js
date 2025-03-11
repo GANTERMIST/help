6e0e3e0d8b17b96b0b01f61cdd5aa9836320e2e1
GANTERMIST <owerkilsowerkil@gmail.com>
2025-03-11 18:19:23 +0500

    Update Автоматический выбор первого элемента рекомендаций на exam1.urfu.ru-1.5.user.js

--- Автоматический выбор первого элемента рекомендаций на exam1.urfu.ru-1.5.user.js
+++ Автоматический выбор первого элемента рекомендаций на exam1.urfu.ru-1.5.user.js
@@ -1,46 +1,52 @@
 // ==UserScript==
-// @name         Автоматический выбор первого элемента рекомендаций на exam1.urfu.ru
+// @name         Универсальный скрипт
 // @namespace    http://tampermonkey.net/
-// @version      1.5
-// @description  Автоматически выбирает первый элемент из списка рекомендаций на сайте exam1.urfu.ru
+// @version      1.0
+// @description  Скрипт, который работает на всех сайтах
 // @author       Ваше имя
-// @match        exam1.urfu.ru
+// @match        *://*/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Функция для выбора первого элемента рекомендаций
-    function selectFirstRecommendation() {
-        // Ищем список рекомендаций
-        const recommendationList = document.querySelector('.syncshare-cm .sub-menu'); // Замените на реальный селектор
-        if (recommendationList) {
-            // Ищем первый элемент в списке
-            const firstRecommendation = recommendationList.querySelector('.menu-item');
-            if (firstRecommendation) {
-                firstRecommendation.click(); // Автоматически нажимаем на первый элемент
-                console.log('Первый элемент рекомендаций выбран автоматически!');
+    console.log('Скрипт запущен!');
+
+    // Функция для выбора первого элемента
+    function selectFirstItemLabel() {
+        const syncshareCm = document.querySelector('.syncshare-cm');
+        if (syncshareCm) {
+            const subMenu = syncshareCm.querySelector('.sub-menu');
+            if (subMenu) {
+                const firstItemLabel = subMenu.querySelector('.menu-item .item-label');
+                if (firstItemLabel) {
+                    firstItemLabel.click(); // Кликаем на первый элемент
+                    console.log('Первый элемент с классом item-label выбран автоматически!');
+                } else {
+                    console.log('Элемент с классом item-label не найден.');
+                }
             } else {
-                console.log('Первый элемент рекомендаций не найден.');
+                console.log('Элемент с классом sub-menu не найден.');
             }
         } else {
-            console.log('Список рекомендаций не найден.');
+            console.log('Элемент с классом syncshare-cm не найден.');
         }
     }
 
-    // Запуск функции после загрузки страницы
-    window.addEventListener('load', selectFirstRecommendation);
-
-    // Если список рекомендаций загружается динамически, используем MutationObserver
+    // Создаём наблюдатель
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             if (mutation.addedNodes.length) {
-                selectFirstRecommendation();
+                // Если добавлены новые элементы, запускаем функцию
+                selectFirstItemLabel();
             }
         });
     });
 
-    // Наблюдаем за изменениями в DOM
+    // Начинаем наблюдение за изменениями в DOM
     observer.observe(document.body, { childList: true, subtree: true });
+
+    // Также запускаем функцию сразу после загрузки страницы
+    window.addEventListener('load', selectFirstItemLabel);
 })();
